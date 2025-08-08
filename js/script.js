4e7681950d7be4702be7497ffca1ad3f1054c649
ShriiiTrackingSolutions <shriitrackingsolutions86@gmail.com>
2025-08-08 09:25:45 +0530

    eAMIL LAYOUT SET

--- js/script.js
+++ js/script.js
@@ -70,7 +70,7 @@ function loadHeaderFooter(callback) {
               </div>
               <div class="footerContacts mt-5">
                 <div class="row">
-                  <div class="col-lg-4">
+                  <div class="col-xl-4 col-lg-auto">
                     <div class="callLinkwrap">
                       <div class="footConIcon">
                         <svg xmlns="http://www.w3.org/2000/svg" width="34px" height="34px" viewBox="0 0 256 256"><path fill="currentColor" d="M146.2 46.45a6 6 0 0 1 7.35-4.25a84.24 84.24 0 0 1 60.25 60.25a6 6 0 0 1-4.25 7.35a6 6 0 0 1-1.55.2a6 6 0 0 1-5.8-4.45a72.34 72.34 0 0 0-51.75-51.75a6 6 0 0 1-4.25-7.35m-3.75 39.35C157 89.68 166.32 99 170.2 113.55A6 6 0 0 0 176 118a6 6 0 0 0 1.55-.2a6 6 0 0 0 4.25-7.35c-5-18.71-17.54-31.25-36.25-36.25a6 6 0 1 0-3.1 11.6m79.44 97A54.25 54.25 0 0 1 168 230C89.7 230 26 166.3 26 88a54.25 54.25 0 0 1 47.17-53.89a14 14 0 0 1 14.56 8.39l21.1 47.1a14 14 0 0 1-1.12 13.28a6 6 0 0 1-.42.57l-21.07 25.06a1.89 1.89 0 0 0 0 1.67c7.66 15.68 24.1 32 40 39.65a1.88 1.88 0 0 0 1.68-.06l24.69-21a5 5 0 0 1 .56-.42a14 14 0 0 1 13.28-1.22l47.24 21.17a14 14 0 0 1 8.22 14.53ZM210 181.32a2 2 0 0 0-1.21-2l-47.25-21.17a1.92 1.92 0 0 0-1.6.1l-24.68 21c-.18.15-.37.29-.56.42a14 14 0 0 1-13.77 1c-18.36-8.87-36.66-27-45.53-45.19a14 14 0 0 1 .91-13.73a5 5 0 0 1 .43-.57l21.05-25.09a2 2 0 0 0 0-1.67L76.74 47.31A2 2 0 0 0 74.9 46h-.23A42.24 42.24 0 0 0 38 88c0 71.68 58.32 130 130 130a42.24 42.24 0 0 0 42-36.68"/></svg>
@@ -82,7 +82,7 @@ function loadHeaderFooter(callback) {
                       </div>
                     </div>
                   </div>
-                  <div class="col-lg-4">
+                  <div class="col-xl-4 col-lg-auto">
                     <div class="callLinkwrap">
                       <div class="footConIcon">
                        <svg xmlns="http://www.w3.org/2000/svg" width="34px" height="34px" viewBox="0 0 24 24"><path fill="currentColor" d="M4.616 19q-.691 0-1.153-.462T3 17.384V6.616q0-.691.463-1.153T4.615 5h14.77q.69 0 1.152.463T21 6.616v10.769q0 .69-.463 1.153T19.385 19zM12 12.116L4 6.885v10.5q0 .269.173.442t.443.173h14.769q.269 0 .442-.173t.173-.443v-10.5zM12 11l7.692-5H4.308zM4 6.885V6v11.385q0 .269.173.442t.443.173H4z"/></svg>
@@ -93,7 +93,7 @@ function loadHeaderFooter(callback) {
                       </div>
                     </div>
                   </div>
-                  <div class="col-lg-4">
+                  <div class="col-xl-4 col-lg-auto addressBox">
                     <div class="callLinkwrap">
                       <div class="footConIcon">
                       <svg xmlns="http://www.w3.org/2000/svg" width="34px" height="34px" viewBox="0 0 32 32"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><circle cx="16" cy="11" r="4"/><path d="M24 15c-3 7-8 15-8 15s-5-8-8-15s2-13 8-13s11 6 8 13"/></g></svg>
